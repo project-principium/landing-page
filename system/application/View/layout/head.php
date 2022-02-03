b14f988032b1632c9c44d3644d5824bacbe5f199
Gabriel Scatolin <gscatolin@outlook.com>
2022-02-02 21:54:46 -0300

    Favicon and seo

--- system/application/View/layout/head.php
+++ system/application/View/layout/head.php
@@ -1,6 +1,8 @@
 <meta charset="UTF-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="Projetos e apresentações feitas sob medida para sua empresa ou seu produto.">
+<meta name="language" content="pt-BR">
 <title><?= empty($title) ? "Project Principium" : "Project Principium | " . htmlspecialchars($title) ?></title>
 
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
@@ -10,5 +12,7 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js" integrity="sha512-pHVGpX7F/27yZ0ISY+VVjyULApbDlD0/X0rgGbTqCE7WFW5MezNTWG/dnhtbBuICzsd0WQPgpE4REBLv+UqChw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
 <script src="https://www.google.com/recaptcha/api.js"></script>
+
+<?php include VIEW_PATH . "layout/favicon.php"; ?>
 
 <link href="/css/app.css" rel="stylesheet"/>
